0e0ece19ce127f806811d4fa5c4cd9f7d5422987
Kevin Taulman <kevin.taulman@icloud.com>
2019-08-20 15:26:25 -0700

    updated server.js with environmental variables

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 //PUTS
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 
-
-app.listen(3000,()=>{
-    console.log('App is running on port 3000')
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`App is running on port ${PORT}`)
 })
